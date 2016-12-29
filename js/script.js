3a37692258479fcb82492a27c5c0a25e597b24c9
iamsail <865605793@qq.com>
2016-12-29 23:09:45 +0800

    gengxin

--- js/script.js
+++ js/script.js
@@ -1,42 +1,31 @@
-﻿/* 
-* Class
-* Copyright (c) 2013 Class	http://gaoge.name
-* E-Mail:g@gaoge.name
-* Date: 2013-08 
-*/ 
-
+﻿
 var wait = 2;
 var jieguo = 0;
 
 $(function(){
 	var time;
 	var font;
-	
+
 	$(".start").show();
-	
 	$(".start").click(function(){
-		//time = setInterval("gaocf()",30);$("#chengfa").animate({fontSize:"50px"});
-		//time = setInterval("gaocf()",30);$("#chengfa").animate({fontSize:"35px"});
-		time = setInterval("gaocf()",30);$("#chengfa").animate({fontSize:"25px"});
-		$(".start,.over").toggle();
+		time = setInterval("gaocf()",30);$("#chengfa_text").animate({fontSize:"25px"});
+		$(".start").hide();
+		$("#come").hide();
+		$("#finish").hide();
+		$(".past_one").hide();
+		$(".about").hide();
+		setTimeout(function(){$(".over").trigger("click")},Math.random()*test.length*1000);
 	});
-	
+
 	$(".over").click(function(){
 		clearInterval(time);
-		for(var i=0;i<test.length;i++ ){
-			if($("#chengfa").html() == test[i]){
-				openFace(Math.floor(Math.random()*4));
-				test.splice(i,1);
-			}
-		}
-		//$("#chengfa").animate({fontSize:"20px"}).animate({fontSize:"35px"});
-		$("#chengfa").animate({fontSize:"20px"}).animate({fontSize:"25px"});
-		//$("#chengfa").animate({fontSize:"20px"}).animate({fontSize:"50px"});
-		$(".start,.over").toggle();
+
+		openFace(Math.round(Math.random()*4));
+		$("#chengfa_text").animate({fontSize:"20px"}).animate({fontSize:"25px"});
 		for( i=1;i<=wait;i++ ){
 			setTimeout("count("+ i + ")",i*1000)
 		}
-		$(".start").hide();$(".count").show();		
+		$(".start").hide();$(".count").show();
 	});
 	
 	$(document).keydown(function(event){
@@ -50,17 +39,49 @@ $(function(){
 				$(".over").click()
 			}
 		};
-		if(event.keyCode == 116){
-			return false;
-		}
-	}); 
+	});
 });
+
+$(function(){
+	$(".past_one").click(function(){
+
+			var more = ["微商","电商","网红，主播","2016年3月30日 魏则西事件","2016年6月23日 英国举行脱欧公投","2016年8月15号 宝强，马蓉，宋喆","2016-10-25 朴槿惠闺蜜门","2016-11-9 美国大选，川普入主白宫","2016-11-17 林丹出轨"];
+			test = more;
+			$("#come").val("");
+	})
+});
+
+
+
+$(function(){
+	$("#finish").click(function(){
+		var text =$("#come").val();
+		temp=text;
+		var arr = temp.split('666');
+		var apple;  //新输入的字符串，化成标准格式后，存储其中
+		apple="["+"\"";
+		for(var i=0;i<arr.length;i++){
+			if(i!==arr.length-1){
+				apple += arr[i]+"\""+","+"\"";
+			}
+			if(i===arr.length-1){
+				apple+=arr[i]+"\""+"]";
+			}
+		}
+		test=arr;
+	});
+});
+
 
 function count(num){
 	if(num == wait){
 		$(".count").hide();
 		$(".start").show();
-	}else{ 
+		$("#come").show();
+		$("#finish").show();
+		$(".past_one").show();
+		$(".about").show();
+	}else{
 		jieguo = wait - num;
 		$(".start").hide();$(".count").show();
 		$(".count").html("启动(" + jieguo + "秒)")
@@ -69,15 +90,16 @@ function count(num){
 
 function gaocf(){
 	var numSj = Math.floor(Math.random()*test.length);
-	var chengfa = test[numSj];
-	$("#chengfa").html(chengfa);
+	var chengfa_text = test[numSj];
+	$("#chengfa_text").html(chengfa_text);
 }
 
 function openFace(obj){
 	$("body").append("<img class='tmp_face' src='images/"+obj+".gif'/>");
 	var winW = $(window).width();
 	var winH = $(window).height();
-	$(".tmp_face").css("top",winH/2-200).fadeIn().animate({"left":winW/2-200},500).delay(1000).fadeOut(function(){
+	$(".tmp_face").css("top",winH/2-100).fadeIn().animate({"left":winW/2-200},500).delay(1000).fadeOut(function(){
 		$(this).remove()
 	});
 }
+
